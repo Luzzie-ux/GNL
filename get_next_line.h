4e3ebe7c3174416592611aeec0f5e0279ab94a2e
Rodrigo Pereira <rodrpere@c1r13s6.42porto.com>
2026-05-13 12:50:42 +0100

    .h file done

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rodrpere <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/13 12:46:36 by rodrpere          #+#    #+#             */
+/*   Updated: 2026/05/13 12:49:50 by rodrpere         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+//prototipos
+
+char	*get_next_line(int fd);
+
+#endif
