051d3339a678a835f05978bf56d4424ceb387a05
Luzzie <estelarodrigoluz@gmail.com>
2026-05-19 19:06:23 +0100

    header up to date

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: rodrpere <rodrpere@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/13 12:46:36 by rodrpere          #+#    #+#             */
-/*   Updated: 2026/05/19 18:45:22 by rodrpere         ###   ########.fr       */
+/*   Updated: 2026/05/19 19:05:46 by rodrpere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,14 @@ size_t	ft_strlen(const char *s);
 int		ft_find_n(char *s);
 char	*fill_buffer(int fd, char *stash);
 char	*get_next_line(int fd);
-char	*next_line(char *stash);
 char	*ft_strdup(const char *s);
-char	*clean_stash(char *stash);
 char	*extract_until_newline(char *stash);
 char	*trim_stash(char *stash);
 char	*ft_strjoin(char *s1, char *s2);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 100
+#  define BUFFER_SIZE 42
 # endif
 
 #endif
